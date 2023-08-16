b87dc73460496e755f3dfa52b56f5e916bafc391
Neezam Eleazar <neezameleazar23@gmail.com>
2023-08-16 15:25:22 +0800

    before sorting has

--- ruby_guess.rb
+++ ruby_guess.rb
@@ -1,11 +1,10 @@
- loop do
-
-    score_points = {}
-    score_name = {}
+  score_name = {}
+  
+  loop do
 
     puts "Welcome,Enter you name"
     player_name = gets.chomp 
-    puts "#{player_name}"
+    puts "Hi #{player_name}"
 
     secret_number = rand(1..100)
     puts secret_number
@@ -13,35 +12,33 @@
     chances = 6
     guesses = [] 
 
-
-
     while tries <= chances
-
-    puts "Guess a number between 1-100"
-    answer = gets.chomp.to_i
-    
-     
-     if answer > secret_number
-      puts "wrong,too high"
-     elsif answer < secret_number
-      puts "number is too low"
-     else 
-     break puts "number is correct"
-     end
-
-     guesses << answer
-     puts "your anwser = [#{answer}]"
+      puts "Guess a number between 1-100"
+      answer = gets.chomp.to_i
       
+      if answer > secret_number
+        puts "wrong,too high"
+      elsif answer < secret_number
+        puts "number is too low"
+      else 
+       break puts "number is correct"
+      end
 
+      guesses << answer
+      puts "your anwser = [#{answer}]"
+        
       tries += 1
     end
 
-    score_name = {:name => player_name}
-    score_points = {:score => tries}
+    score_name[player_name] = tries
 
+    puts "SCOREBOARD"
+    puts score_name.class
+  
     puts "high score" 
-    puts  score_name [:name]
-    puts score_points [:score]
+    score_name.each do |player_name,tries|
+    puts "#{player_name}: - #{tries}"
+  end
     puts "do you want to play again?"
     puts "y - Yes"
     puts "n - NO"
