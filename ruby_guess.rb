9d3ed5ce9fee1187a7ffd80a9f436096c5071ee9
Neezam Eleazar <neezameleazar23@gmail.com>
2023-08-16 17:14:17 +0800

    level time

--- ruby_guess.rb
+++ ruby_guess.rb
@@ -1,9 +1,10 @@
-  @score_name = {}
+@score_name = {}
 
 def game_difficulty()
 puts "Choose your difficulty"
-puts"Easy - Medium - Hard"
+puts"easy - medium - hard"
 @decided_difficulty = gets.chomp
+if @decided_difficulty == "easy"
 end
 
   def name_player()
@@ -12,7 +13,7 @@ end
   puts "Hi #{@player_name}"
   end
 
-  def game_value_number ()
+  def game_value_number (easy)
    @secret_number = rand(1..100)
    puts @secret_number
    @tries = 1
@@ -20,9 +21,37 @@ end
    @guesses = []
   end
 
- def game_value()
+  def game_value_number (medium)
+   @secret_number = rand(1..200)
+   puts @secret_number
+   @tries = 1
+   @chances = 6
+   @guesses = []
+  end
+
+ def game_value(easy)
     while @tries <= @chances
       puts "Guess a number between 1-100"
+      @answer = gets.chomp.to_i
+      
+        if @answer > @secret_number
+        puts "wrong,too high"
+        elsif @answer < @secret_number
+        puts "number is too low"
+        else 
+        break puts "number is correct"
+      end
+      
+       @guesses << @answer
+      puts "your anwser = [#{@answer}]"
+        
+      @tries += 1
+    end
+  end
+
+  def game_value(medium)
+    while @tries <= @chances
+      puts "Guess a number between 1-200"
       @answer = gets.chomp.to_i
       
         if @answer > @secret_number
@@ -64,8 +93,10 @@ def run_game()
   loop do
     game_difficulty()
     name_player()
-    game_value_number()
-    game_value()
+    game_value_number(easy)
+    game_value_number(medium)
+    game_value(easy)
+    game_value(medium)
     scoreboard_values()
     restart_game()
     break if @restart !="y"
