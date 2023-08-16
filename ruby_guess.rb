20eed21c66d6dcf74fb58a1d6a8cf3b196d6e3f5
Neezam Eleazar <neezameleazar23@gmail.com>
2023-08-16 14:03:38 +0800

    wednesday

--- ruby_guess.rb
+++ ruby_guess.rb
@@ -1,55 +1,58 @@
+ loop do
 
-    loop do
+    score_points = {}
+    score_name = {}
 
-    puts "Welcome ,Do you want to play a guessing game?"
-    player_answer = gets.chomp.downcase
-  if player_answer => "yes"
-    break if player_answer = ["no","No","NO","n"]
-      puts "#{player_answer}, Enter your name."
+    puts "Welcome,Enter you name"
     player_name = gets.chomp 
     puts "#{player_name}"
-    
-
-
-
-
-
-
 
     secret_number = rand(1..100)
     puts secret_number
     tries = 1
-    # how many times need to run.variable (counter)
     chances = 6
-    # at what number you want it to stop
     guesses = [] 
 
 
-    while tries <= chances
-    #roof aka start of the block
 
+    while tries <= chances
 
     puts "Guess a number between 1-100"
     answer = gets.chomp.to_i
-    guesses << answer
+    
      
      if answer > secret_number
       puts "wrong,too high"
      elsif answer < secret_number
       puts "number is too low"
      else 
-      break puts "number is correct"
+     break puts "number is correct"
      end
-    puts"guesses = #{guesses}"
 
-     tries += 1
-   end
+     guesses << answer
+     puts "your anwser = [#{answer}]"
+      
+
+      tries += 1
+    end
+
+    score_name = {:name => player_name}
+    score_points = {:score => tries}
+
+    puts "high score" 
+    puts  score_name [:name]
+    puts score_points [:score]
+    puts "do you want to play again?"
+    puts "y - Yes"
+    puts "n - NO"
+
+    restart = gets.chomp.downcase
+    break if restart !="y"
+
+  end
 
 
-     
-   #+= is multiply the number ? 
-end
-   
+
    
 
 
