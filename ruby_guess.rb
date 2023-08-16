98d03dd4272e1fd399b771bad2b1f8cc5c9d6afc
Neezam Eleazar <neezameleazar23@gmail.com>
2023-08-16 10:47:39 +0800

    trying to make intro work with yes to continue and no to stop

--- ruby_guess.rb
+++ ruby_guess.rb
@@ -1,38 +1,58 @@
-  secret_number = rand(1..100)
-  puts secret_number
-  tries = 1
-  # how many times need to run.variable (counter)
-  chances = 6
-  # at what number you want it to stop
-  guesses = [] #third commit testing
+
+    loop do
+
+    puts "Welcome ,Do you want to play a guessing game?"
+    player_answer = gets.chomp.downcase
+  if player_answer => "yes"
+    break if player_answer = ["no","No","NO","n"]
+      puts "#{player_answer}, Enter your name."
+    player_name = gets.chomp 
+    puts "#{player_name}"
+    
 
 
-  while tries <= chances
-  #roof aka start of the block
 
 
-  puts "Guess a number between 1-100"
-  answer = gets.chomp.to_i
-  guesses << answer
-   
-   if answer > secret_number
-    puts "wrong,too high"
-   elsif answer < secret_number
-    puts "number is too low"
-   else 
-    break puts "number is correct"
+
+
+
+    secret_number = rand(1..100)
+    puts secret_number
+    tries = 1
+    # how many times need to run.variable (counter)
+    chances = 6
+    # at what number you want it to stop
+    guesses = [] 
+
+
+    while tries <= chances
+    #roof aka start of the block
+
+
+    puts "Guess a number between 1-100"
+    answer = gets.chomp.to_i
+    guesses << answer
+     
+     if answer > secret_number
+      puts "wrong,too high"
+     elsif answer < secret_number
+      puts "number is too low"
+     else 
+      break puts "number is correct"
+     end
+    puts"guesses = #{guesses}"
+
+     tries += 1
    end
-  puts"guesses = #{guesses}"
 
-   tries += 1
- end
+
+     
+   #+= is multiply the number ? 
+end
    
- #+= is multiply the number ? 
-
- 
- 
+   
 
 
 
 
- 
+   
